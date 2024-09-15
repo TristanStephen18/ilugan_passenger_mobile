3071a7dbee721bfef0df1b6e9f81356f979066af
TristanStephen18 <trasco_21ur1406@psu.edu.ph>
2024-09-15 13:35:37 +0800

    connected to firebase database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:ilugan_passenger_mobile_app/screens/splashscreen.dart';
 
-void main(){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Ilugan());
 }
 
